e022c08b155b8a44b6803948d54a7887c20cad00
Brendan Lyons <lyonsbc@fusion.gat.com>
2024-07-25 15:35:42 -0700

    Create test for Shot from gEQDSK (#59)

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,6 +16,7 @@ const psi_fe = TEQUILA.FE(x, (x .^ 2) .- 2.0)
 @testset "TEQUILA.jl" begin
     @test Shot(10, 12, bnd; dP_dψ=(Pp, :poloidal), F_dF_dψ=(FFp, :toroidal), Pbnd, Fbnd, Ip_target) isa Shot
     @test Shot(12, 10, bnd, psi ; P=(P, :toroidal), Jt=(J, :poloidal), Pbnd, Fbnd) isa Shot
+    @test Shot(11, 5, 6, (@__DIR__) * "/g_chease_mxh_d3d") isa Shot
     shot = Shot(11, 11, bnd, psi_fe; P=(P, :toroidal), Jt_R=(J, :toroidal), Pbnd, Fbnd, Ip_target)
     @test solve(shot, 5) isa Shot
     refill = solve(shot, 21; relax=0.5, tol=1e-3, debug=true, dP_dψ=(Pp, :toroidal), Jt=(J, :toroidal))

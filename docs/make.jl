c20373c766b2435a7a652836552436aa56f5e123
Orso Meneghini <orso82@gmail.com>
2024-10-25 12:12:52 -0700

    add analytics

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ end
 
 makedocs(;
     modules=[TEQUILA],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(;analytics="G-65D8V8C8VQ"),
     sitename="TEQUILA",
     checkdocs=:none,
     pages=["index.md", "api.md", "License" => "license.md", "Notice" => "notice.md"],
